595992e54651fd11ffde9cdf8140e6ad807baf18
utsavsingh-bit <utsavsingh556677@gmail.com>
2026-09-20 12:09:55 +0530

    Time: 1 ms (99.82%), Space: 46.5 MB (92.27%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+       String[] answer = new String[n];
+        for(int i =1; i<= n;i++){
+            if(i%3==0 && i%5==0){
+                answer[i-1] = "FizzBuzz";
+            }else if(i%3==0){
+                answer[i-1] = "Fizz";
+            }else if(i%5==0){
+                answer[i-1] = "Buzz";
+            }else{
+                answer[i-1]=String.valueOf(i);
+            }
+        }
+        return Arrays.asList(answer);
+    }
+}
